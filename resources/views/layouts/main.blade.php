76b0a2f2ab31ea0bb011b80e4667362dcdd49a23
HaruSasaki <73406832+HaruSasaki@users.noreply.github.com>
2022-03-17 18:32:39 +0700

    Add footer credit

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -36,4 +36,10 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 
+<footer>
+    <div class="container mt-4" style="display: flex; justify-content: center; align-item:center;">
+    <p> Made with love by <i>Iqbal Abdurrahman</i> (XI RPL 1) - <a href="mailto:iqbalabdurrahman04@gmail.com">Contact Me!</a></p>
+    </div>
+</footer>
+
 </html>
